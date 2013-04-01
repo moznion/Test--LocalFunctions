c1453014c7550f07355c06c3dfd6d9901fb41942
moznion <moznion@gmail.com>
2013-04-02 00:33:10 +0900

    Add docs.

--- lib/Test/LocalFunctions.pm
+++ lib/Test/LocalFunctions.pm
@@ -2,7 +2,6 @@ package Test::LocalFunctions;
 
 use strict;
 use warnings;
-use Carp;
 use ExtUtils::Manifest qw/maniread/;
 use Sub::Identify qw/stash_name/;
 use PPI::Document;
@@ -57,7 +56,7 @@ sub _local_functions_ok {
 }
 
 sub _check_local_functions {
-    my ( $builder, $file, $args ) = @_;
+    my ( $builder, $file ) = @_;    # append $args later?
 
     my $fail = 0;
 
@@ -169,108 +168,65 @@ __END__
 
 =head1 NAME
 
-Test::LocalFunctions - [One line description of module's purpose here]
+Test::LocalFunctions - Detects unused local functions
 
 
 =head1 VERSION
 
-This document describes Test::LocalFunctions version 0.0.1
+This document describes Test::LocalFunctions version 0.01
 
 
 =head1 SYNOPSIS
 
     use Test::LocalFunctions;
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
+    all_local_functions_ok(); # check modules that are listed in MANIFEST
 
 
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+Test::LocalFunctions finds unused local functions to clean up the source code.
+(Local function means the function which name starts from underscore.)
 
 
-=head1 INTERFACE
-
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+=head1 METHODS
 
 =over
 
-=item C<< Error message here, perhaps with %s placeholders >>
+=item C<< all_local_functions_ok >>
 
-[Description of error here]
+This is a test function which finds unused variables from modules that are listed in MANIFEST file.
 
-=item C<< Another error message here >>
+=item C<< local_functions_ok >>
 
-[Description of error here]
-
-[Et cetera, et cetera]
+This is a test function which finds unused variables from specified source code.
+This function requires an argument which is the path to source file.
 
 =back
 
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-
 Test::LocalFunctions requires no configuration files or environment variables.
 
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
+PPI (version 1.215 or later)
 
-None.
+Sub::Identify (version 0.04 or later)
+
+Test::Builder::Module (version 0.98 or later)
+
+Test::Builder::Tester (version 1.22 or later)
 
 
 =head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
 
 None reported.
 
 
 =head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
 
 No bugs have been reported.
 
