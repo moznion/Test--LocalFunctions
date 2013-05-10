379a210d95b0b857a042bd0c090fbd5013c3515b
moznion <moznion@gmail.com>
2013-05-10 18:53:11 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 58350dd..1a556ca 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Test::LocalFunctions
    
    +0.04    Fri May 10 18:49:27 +0900 2013
    +        - Tiny fixes
    +
     0.03    Thu Apr 04 04:14:37 +0900 2013
             - Removed a dependency on Module::Build::Pluggable::CPANfile

--- lib/Test/LocalFunctions.pm
+++ lib/Test/LocalFunctions.pm
@@ -8,7 +8,7 @@ use Sub::Identify qw/stash_name/;
 use PPI::Document;
 use PPI::Dumper;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 our @EXPORT  = qw/all_local_functions_ok local_functions_ok/;
 
 use parent qw/Test::Builder::Module/;
@@ -172,7 +172,7 @@ Test::LocalFunctions - Detects unused local functions
 
 =head1 VERSION
 
-This document describes Test::LocalFunctions version 0.03
+This document describes Test::LocalFunctions version 0.04
 
 
 =head1 SYNOPSIS
